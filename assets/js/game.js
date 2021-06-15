20a782d0bd464fdbcf92e2ec0af11241f1012a22
Briofita09 <felipe.osorio.santos@hotmail.com>
2021-06-15 12:19:04 -0300

    transportadas classes pro index.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -52,7 +52,7 @@ document.querySelectorAll(".cell").forEach((tile) => {
   }
   tile.onclick = function () {
     tile.classList.add("ship");
-    verify();
+    verify("a1");
   };
 });
 
@@ -76,68 +76,6 @@ let gameBoard = [
 ];
 
 console.log(gameBoard);
-
-// criando navios
-
-class Ship {
-  constructor(size, position) {
-    this.size = size;
-  }
-}
-
-//criando destroyer com tamanho de 4 tiles (3 desses estarão no tabuleiro)
-
-class Destroyer extends Ship {
-  constructor(size, position) {
-    super(size, position);
-    this.size = 4;
-  }
-
-  placeDestroyer() {
-    return tile.classList.add("ship");
-  }
-}
-
-//criando porta-aviões com tamanho de 5 tiles (1 desse estará no tabuleiro)
-
-class AircraftCarrier extends Ship {
-  constructor(size, position) {
-    super(size, position);
-    this.size = 5;
-  }
-  placeAircraftCarrier() {
-    return tile.classList.add("ship");
-  }
-}
-
-//criando cruzador com tamanho de 3 tiles (4 desses estarão no tabuleiro)
-
-class Cruiser extends Ship {
-  constructor(size, position) {
-    super(size, position);
-    this.size = 3;
-  }
-  placeCruiser() {
-    return tile.classList.add("ship");
-  }
-}
-
-//criando a classe tiro
-
-class Shot {
-  constructor(local) {
-    this.local = local;
-  }
-
-  //verificando se acertou ou errou
-  verify(local) {
-    if (local === tile.classList.contains("ship")) {
-      tile.classList.add("correct");
-    } else {
-      tile.classList.add("miss");
-    }
-  }
-}
 
 for (let i = 0; i < gameBoard.length; i++) {
   for (let j = 0; j < gameBoard[i].length; j++) {
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,62 @@
+// classes
+// criando navios
 
+class Ship {
+  constructor(size, position) {
+    this.size = size;
+  }
+}
+
+//criando destroyer com tamanho de 4 tiles (3 desses estarão no tabuleiro)
+
+class Destroyer extends Ship {
+  constructor(size, position) {
+    super(size, position);
+    this.size = 4;
+  }
+
+  placeDestroyer() {
+    return tile.classList.add("ship");
+  }
+}
+
+//criando porta-aviões com tamanho de 5 tiles (1 desse estará no tabuleiro)
+
+class AircraftCarrier extends Ship {
+  constructor(size, position) {
+    super(size, position);
+    this.size = 5;
+  }
+  placeAircraftCarrier() {
+    return tile.classList.add("ship");
+  }
+}
+
+//criando cruzador com tamanho de 3 tiles (4 desses estarão no tabuleiro)
+
+class Cruiser extends Ship {
+  constructor(size, position) {
+    super(size, position);
+    this.size = 3;
+  }
+  placeCruiser() {
+    return tile.classList.add("ship");
+  }
+}
+
+//criando a classe tiro
+
+class Shot {
+  constructor(local) {
+    this.local = local;
+  }
+
+  //verificando se acertou ou errou
+  verify(local) {
+    if (local === tile.classList.contains("ship")) {
+      tile.classList.add("correct");
+    } else {
+      tile.classList.add("miss");
+    }
+  }
+}
