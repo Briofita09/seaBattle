e31169bd12c3ab40ff9439e1267470e1c0c518a6
Briofita09 <felipe.osorio.santos@hotmail.com>
2021-06-14 21:06:20 -0300

    iniciada a criacao do js

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+let table = document.querySelector("tbody");
+
+function clicar() {
+  let tile = document.querySelectorAll("cell");
+  console.log(tile);
+}
+//console.log(tile);
+document.addEventListener("click", function () {
+  clicar();
+});
